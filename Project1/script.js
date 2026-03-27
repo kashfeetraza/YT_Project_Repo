c0eeae0f4ad7299a4df99af17abf829281d8d15c
Kashfeet Raza <kashfeetraza@gmail.com>
2026-03-27 19:51:58 +0500

    This is the Project of changing color in Web Page

---
+++ Project1/script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body')
+const button = document.querySelectorAll('.button')
+
+button.forEach(function (button){
+    // console.log(button);
+    button.addEventListener('click', function(event){
+        console.log(event)
+        console.log(event.target)
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'green'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'red'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id === 'black'){
+            body.style.backgroundColor = event.target.id
+        }
+    })
+})
